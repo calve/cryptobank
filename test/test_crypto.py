70e6efc1a62a4314ef285bcf37ac19a82c244c2c
calve <calvinh34@gmail.com>
2015-09-23 16:52:39 +0200

    More tests

--- test/test_crypto.py
+++ test/test_crypto.py
@@ -10,3 +10,10 @@ class TestCrypto(unittest.TestCase):
         self.assertTrue(key.verify(random_words, signature))
         self.assertFalse(key.verify("plopplopplop", signature))
         self.assertFalse(key.verify(random_words, "notasignature"))
+
+    def test_key_gen(self):
+        key1 = generate_keys()
+        key2 = generate_keys()
+        self.assertNotEquals(key1.e, key1.d)
+        self.assertNotEquals(key1.e, key2.e)
+        self.assertNotEquals(key1.d, key2.d)
