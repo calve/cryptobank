319b1c135eca5d57426ae7b7abe4648040e2153e
rkouere <rkouere@gmail.com>
2015-09-27 12:01:26 +0200

    the customer can now produce a signed check

--- cryptobank/customer.py
+++ cryptobank/customer.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 from monrsa.crypto import Key
-from monrsa.tools import save_rsa_keys, unserialize
+from monrsa.tools import save_rsa_keys, serialize
 import sys
 import argparse
 import json
@@ -8,22 +8,32 @@ import json
 
 def import_check(path):
     """
-    Import the check to sign as a json dictionary
+    Import the check to sign
     """
     
     with open(path, "r") as file_:
         check_str = file_.readline()
-    return unserialize(check_str))
+    return check_str 
     
 
 def sign_check(arguments):
-    private_key = arguments[0]
-    check = import_check(arguments[1]) 
-    
-    
-    #privatekey = Key.import_key(privatekey_string)
-    #signature = privatekey.sign(transaction_string)
-    #return signature
+    """
+    Import the check to sign
+    Import the customer's private key
+    Sign the check
+    Create a new check that contains : 
+        - the base64 version of the check
+        - the signature of the base64
+    Prints a base64 signed check
+    """
+    check = import_check(arguments)
+    privatekey = Key.import_key_from_path("customer.key")
+    signature = privatekey.sign(check).decode()
+    signed_check = {
+        "base64_check": check,
+        "signature": signature
+    }
+    print(serialize(signed_check).decode())
 
 
 
@@ -33,17 +43,16 @@ def main():
     argparser.add_argument("--generate-keys",  # This is also a binary option
                            action="store_true",
                            help="Generates the customer's keys")
-    argparser.add_argument("--private-key",  # This is also a binary option
-                           nargs=2,
-                           metavar=('PRIVATE-KEY', 'CHECK-TO-SIGN'),
+    argparser.add_argument("--sign-check",  # This is also a binary option
+                           metavar=('CHECK-TO-SIGN'),
                            help="Takes a the customer's signature and a check and sign it.")
     arguments = argparser.parse_args()
 
     # Now do things depending of the collected arguments
     if arguments.generate_keys:
         save_rsa_keys("customer.pubkey", "customer.key")
-    if arguments.private_key:
-        sign_check(arguments.private_key)
+    if arguments.sign_check:
+        sign_check(arguments.sign_check)
 
 # This is a Python's special:
 # The only way to tell wether we are running the program as a binary,
