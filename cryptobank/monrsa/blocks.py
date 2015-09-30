c3904e74558fe8167713e82ddcd2503da8b3931f
calve <calvinh34@gmail.com>
2015-09-30 17:21:44 +0200

    Implement blocks

--- cryptobank/monrsa/blocks.py
+++ cryptobank/monrsa/blocks.py
@@ -1,11 +1,35 @@
-def split(rawdate, size):
+def split(rawdata, size):
     """
-    Split rawdata in block of size bits
+    Split rawdata in block of size bytes
     """
-    return []
+    result = []
+    for i in range(0, len(rawdata), size):
+        if i + size <= len(rawdata):
+            result.append(rawdata[i:i+size])
+        else:
+            # Looks likes we need to add padding
+            modulo = len(rawdata) % size
+            padd = size - modulo
+            padded = rawdata[i:i+modulo] + (bytes([padd]) * padd)
+            # print("padded : {}".format(padded))
+            result.append(padded)
+            # print("need to pad")
+    return result
 
 def assemble(blocks):
     """
     Reassemble blocks
     """
-    return ""
+    result = bytearray()
+    for block in blocks:
+        for byte in block:
+            result.append(byte)
+    # Was it padded ?
+    if result[-1] < ord(" "):
+        number_of_bytes = result[-1]
+        # Verify that all padded bytes are actually padded bytes
+        padding = result[-number_of_bytes:]
+        if padding != bytes([number_of_bytes]) * number_of_bytes:
+            raise Exception("Error un-padding")
+        return result[:-number_of_bytes]
+    return result
--- cryptobank/test/unittests/test_blocks.py
+++ cryptobank/test/unittests/test_blocks.py
@@ -5,6 +5,30 @@ from cryptobank.test.unittests import randomword
 
 class TestBlocks(unittest.TestCase):
 
-    def test_random_words(self):
-        word = randomword(4096)
-        self.assertEquals(assemble(split(word, 16)), word)
+    def test_long_word(self):
+        rawdata = bytes(randomword(4096).encode())
+        blocks = split(rawdata, 15)
+        self.assertEqual(assemble(blocks), rawdata)
+        self.assertNotEqual(assemble(blocks[:-1]), rawdata)
+
+    def test_short_word(self):
+        rawdata = bytes(randomword(40).encode())
+        blocks = split(rawdata, 15)
+        self.assertEqual(assemble(blocks), rawdata)
+        self.assertNotEqual(assemble(blocks[:-1]), rawdata)
+
+    def test_padding(self):
+        rawdata = bytes(randomword(18).encode())
+        blocks = split(rawdata, 16)
+        self.assertEqual(blocks[-1][-1], 14)
+
+    def test_split(self):
+        rawdata = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        blocks = split(rawdata, 16)
+        self.assertEqual(b"ABCDEFGHIJKLMNOP", blocks[0])
+        self.assertEqual(b"QRSTUVWXYZ\6\6\6\6\6\6", blocks[1])
+
+    def test_resplit(self):
+        rawdata = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        blocks = split(rawdata, 5)
+        self.assertEqual([b'ABCDE', b'FGHIJ', b'KLMNO', b'PQRST', b'UVWXY', b'Z\4\4\4\4'], blocks)
