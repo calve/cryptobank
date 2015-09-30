86134f61fb34227d19fc6b68ff988d9880677201
rkouere <rkouere@gmail.com>
2015-09-30 17:03:37 +0200

    verif authentification ok

--- cryptobank/test/functionalTests/test_clientauth.py
+++ cryptobank/test/functionalTests/test_clientauth.py
@@ -17,10 +17,15 @@ class TestCrypto(unittest.TestCase):
         """
         Test that we only accept a client with a signature from the bank
         """
-        bankKey = Key.import_key_from_path("bank.key")
-        
-        signature = import_key("customer.signedkey")
-        with open("customer.pubkey", "r") as file_:
+        bankKey = Key.import_key_from_path("./cryptobank/test/functionalTests/keys/bank.key")
+        bankKeyFalse = Key.import_key_from_path("./cryptobank/test/functionalTests/keys/bankFalse.key")
+        signature = import_key("./cryptobank/test/functionalTests/keys/customer.signedkey")
+        signature_false = import_key("./cryptobank/test/functionalTests/keys/customerFalse.signedkey")
+        with open("./cryptobank/test/functionalTests/keys/customer.pubkey", "r") as file_:
             customer_key = file_.read()
-        self.assertTrue(bankKey.verify(customer_key, signature))
+        
 
+        self.assertTrue(bankKey.verify(customer_key, signature))
+        self.assertFalse(bankKey.verify(customer_key, signature_false))
+        self.assertFalse(bankKeyFalse.verify(customer_key, signature))
+        
