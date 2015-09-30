69c3c1374498e690e4cadc3ba1efca2bf1f70e12
calve <calvinh34@gmail.com>
2015-10-01 00:41:44 +0200

    Fix padding

--- cryptobank/test/unittests/test_blocks.py
+++ cryptobank/test/unittests/test_blocks.py
@@ -6,8 +6,14 @@ from cryptobank.test.unittests import randomword
 class TestBlocks(unittest.TestCase):
 
     def test_long_word(self):
-        rawdata = bytes(randomword(4096).encode())
+        rawdata = bytes(randomword(30).encode())
         blocks = split(rawdata, 15)
+        self.assertEqual(assemble(blocks), rawdata)
+        self.assertNotEqual(assemble(blocks[:-1]), rawdata)
+
+    def test_long_word_long_split(self):
+        rawdata = bytes(randomword(4098).encode())
+        blocks = split(rawdata, 256)
         self.assertEqual(assemble(blocks), rawdata)
         self.assertNotEqual(assemble(blocks[:-1]), rawdata)
 
@@ -17,18 +23,28 @@ class TestBlocks(unittest.TestCase):
         self.assertEqual(assemble(blocks), rawdata)
         self.assertNotEqual(assemble(blocks[:-1]), rawdata)
 
-    def test_padding(self):
-        rawdata = bytes(randomword(18).encode())
-        blocks = split(rawdata, 16)
-        self.assertEqual(blocks[-1][-1], 14)
+    def test_padd_one(self):
+        rawdata = bytes(randomword(29).encode())
+        blocks = split(rawdata, 10)
+        self.assertEqual(assemble(blocks), rawdata)
+
+    def test_padd_two(self):
+        rawdata = bytes(randomword(28).encode())
+        blocks = split(rawdata, 10)
+        self.assertEqual(assemble(blocks), rawdata)
+
+    def test_no_padd(self):
+        rawdata = bytes(randomword(30).encode())
+        blocks = split(rawdata, 10)
+        self.assertEqual(assemble(blocks), rawdata)
 
     def test_split(self):
         rawdata = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
         blocks = split(rawdata, 16)
         self.assertEqual(b"ABCDEFGHIJKLMNOP", blocks[0])
-        self.assertEqual(b"QRSTUVWXYZ\6\6\6\6\6\6", blocks[1])
+        self.assertEqual(b"QRSTUVWXYZ\0\6\0\0\0\0", blocks[1])
 
     def test_resplit(self):
         rawdata = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
         blocks = split(rawdata, 5)
-        self.assertEqual([b'ABCDE', b'FGHIJ', b'KLMNO', b'PQRST', b'UVWXY', b'Z\4\4\4\4'], blocks)
+        self.assertEqual([b'ABCDE', b'FGHIJ', b'KLMNO', b'PQRST', b'UVWXY', b'Z\0\4\0\0'], blocks)
