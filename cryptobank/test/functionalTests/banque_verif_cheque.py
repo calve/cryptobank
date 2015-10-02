6d933df1a160398d41fff0c516639951fbca42d4
rkouere <rkouere@gmail.com>
2015-10-02 15:59:52 +0200

    tests verification que l'on ne peut changer le cheque ok + creation d'un cheque qui a le meme token

--- cryptobank/test/functionalTests/banque_verif_cheque.py
+++ cryptobank/test/functionalTests/banque_verif_cheque.py
@@ -28,6 +28,18 @@ class TestCrypto(unittest.TestCase):
         # sauvegarde du check dans la db
         store_check(base64_check, path + "bank.db")
         
+        #verification que il ne peut être re-encaisse
+        with self.assertRaises(SystemExit) as cm:
+            verify_check_first(dic_check, path+"bank.db")
+        the_exception = cm.exception
+        self.assertEqual(the_exception.code, 1)
+
+        # verif utilisation du meme token pour deux cheque avec differents token
+        with open(path + "checkSameToken.json", "r") as file_:
+            signed_check_same_token = unserialize(file_.read())
+        base64_check = signed_check_same_token["base64_check"]
+        dic_check = unserialize(base64_check)
+        #verification que il ne peut être re-encaisse
         with self.assertRaises(SystemExit) as cm:
             verify_check_first(dic_check, path+"bank.db")
         the_exception = cm.exception
