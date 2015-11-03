25d1333141d134e6fe7aa6d089925d9515ab676e
rkouere <rkouere@gmail.com>
2015-11-03 11:41:35 +0100

    verif client ne peut changer le cheque sans que le marchant ne s'en rende compte OK

--- shell/command.sh
+++ shell/command.sh
@@ -19,10 +19,10 @@ function normalTest {
     echo "[bank] signing the customer's key"
     ./bank.sh -b customer.pubkey bank.key customer.pubkey.signed
 
-    echo "[marchant] Le marchant crée un chèque avec"
+    echo "[marchant] Le marchant crée un chèque avec le montant, son id et sa clef signe"
     ./merchant.sh -a 40 1 customer.pubkey.signed > transaction.txt
 
-    echo "[client] Le client signe avec sa clef privée le message contenant"
+    echo "[client] Le client signe avec sa clef privée le cheque"
     ./customer.sh -c transaction.txt cheque.txt
 
     echo "[marchant] Le marchant vérifie grâce a la clef publique de la banque que la signature du client est valide"
@@ -38,8 +38,38 @@ function normalTest {
     ./bank.sh -d bank.db cheque.txt
 }
 
+function test1 {
+    # qu'un client ne peut changer le cheque sans que le marchant ne s'en rende compte 
+    echo "[bank] creation des clefs du client"
+    ./bank.sh -a
+
+    echo "[customer] creation des clefs du client"
+    ./customer.sh -a
+
+    echo "[bank] signing the customer's key"
+    ./bank.sh -b customer.pubkey bank.key customer.pubkey.signed
+
+    echo "[marchant] Le marchant crée un chèque avec le montant, son id et sa clef signe"
+    ./merchant.sh -a 40 1 customer.pubkey.signed > transaction.txt
+    
+    echo "[marchant] changement du cheque"
+    cp transaction.txt transaction2.txt
+    sed -i -e 's/40/41/g' transaction2.txt
+
+    echo "[client] Le client signe avec sa clef privée le cheque"
+    ./customer.sh -c transaction2.txt cheque.txt
+    
+    echo "[marchant] Le marchant vérifie grâce a la clef publique de la banque que la signature du client est valide"
+    ./merchant.sh -b cheque.txt customer.pubkey bank.pubkey transaction2.txt
+
+    echo "[marchant] Le marchant vérifie que le contenu du chèque est ce qu'il attend (montant, ordre, nombre aléatoire)"
+    ./merchant.sh -c transaction.txt cheque.txt
+
+}
+
 function usage {
     echo "-a: normal transaction"
+    echo "-b: test1"
     echo "-R: clean dir"
 }
 
@@ -47,12 +77,16 @@ if [ -z "$1" ]
   then 
     usage 
   else 
-    while getopts “aR” OPTION 
+    while getopts “abR” OPTION 
     do 
         case $OPTION in 
             a) 
                 echo "creation des clefs de la banque"
                 normalTest
+                ;; 
+            b) 
+                echo "creation des clefs de la banque"
+                test1
                 ;; 
             R) 
                 echo "Cleaning dir"
